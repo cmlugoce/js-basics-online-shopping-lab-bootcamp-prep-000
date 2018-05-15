7d19bde11356c64f6d9490ebdd4c9954dc3e7e29
Cristina Lugo <cmlugoce@syr.edu>
2018-05-15 20:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+<<<<<<< HEAD
   var price = Math.floor(Math.random() * 100) + 1;
   var newItem = {
     itemName: item,
@@ -27,11 +28,35 @@ function removeFromCart(item) {
     }
   }
   return "That item is not in your cart.";
+=======
+  var itemAndPrice = {};
+  var price = Math.floor(Math.random() * 100) + 1;
+  itemAndPrice[item] = price;
+  cart.push(itemAndPrice);
+  console.log(item + " has been added to your cart.");
+  return cart;
+
+}
+
+function removeFromCart(item) {
+  var itemInCart = false
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart.splice(i, 1);
+   }
+  }
+  if (!itemInCart) {
+    console.log("That is not in your cart.");
+  }
+  return cart
+>>>>>>> 551f35f3195ac618cc9ed34b97fb3c3997bf1e20
 }
 
 function total() {
   // iterates over cart items & adds their prices
   //returns total;
+<<<<<<< HEAD
   let totalCost = 0;
   for (let i = 0; i < cart.length; i++){
     totalCost += cart[i].itemPrice;
@@ -71,3 +96,44 @@ function viewCart(){
     return "Sorry, we don't have a credit card on file for you.";
   }
 }
+=======
+    var totalCost = 0;
+
+    for(var i = 0; i < getCart().length; i++){
+      for(var cost in getCart()[i]){
+        totalCost += getCart()[i][cost];
+      }
+    }
+    return totalCost;
+}
+
+
+function viewCart(){
+   if (cart.length === 0){
+     console.log("Yourping cart is empty.")
+   } else {
+     var itemsAndPrices = [];
+
+     for (var i in cart){
+       var itemAndPrice = cart[i];
+       for (var key in itemAndPrice){
+         itemsAndPrices.push(key + " at $" + itemAndPrice[key])
+       }
+     }
+     var cartItems = itemsAndPrices.join(", ");
+     console.log("In cart, you have " + cartItems + ".")
+   }
+ }
+
+
+ function placeOrder(cc){
+    if(!cc){
+     console.log("Wet have a credit card on file for you to place your order.");
+   } else {
+     console.log(`Your total cost is $${total()}, which will be charged to the card ${cc}.`);
+     cart = [];
+
+   }
+
+  }
+>>>>>>> 551f35f3195ac618cc9ed34b97fb3c3997bf1e20
